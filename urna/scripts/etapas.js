694a11c7fb69bec09cda1ddb217c449ebd2d3a01
eduardo guadagnin <eduardo.guadagnin15@gmail.com>
2022-06-27 09:51:04 -0300

    primeiro commit do código(pode conter bugs)

---
+++ urna/scripts/etapas.js
@@ -0,0 +1,62 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'50500',
+                nome: 'Pedro Ruas',
+                partido: 'PSOL',
+                vice: '',
+                fotos:[
+                    {url:'50500.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero:'50123',
+                nome: 'Matheus Gomes',
+                partido: 'PSOL',
+                vice: '',
+                fotos:[
+                    {url:'50123.jpg', legenda:'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+       titulo: 'PRESIDENTE',
+       numeros: 2,
+       candidatos: [
+           {
+                numero: '13',
+                nome: 'Luis inacio lula da silva',
+                partido: "PT",
+                vice: 'Mao',
+                fotos:[
+                    {url:'13.jpg', legenda:'Presidente'},
+                    {url:'13_2.jpg', legenda:'Vice-Presidente', small: true}
+                ]
+           },
+           {
+                numero: '12',
+                nome: 'Ciro gomes',
+                partido: "PDT",
+                vice: 'Vai saber',
+                fotos:[
+                    {url:'12.jpg', legenda:'Presidente'},
+                    {url:'12_2.jpg', legenda:'Vice-Presidente', small: true}
+                ]
+           },
+           {
+                numero: '17',
+                nome: 'Jair *********',
+                partido: "fodase",
+                vice: 'Queiroz',
+                fotos:[
+                    {url:'17.jpg', legenda:'Presidente'},
+                    {url:'17_2.jpg', legenda:'Vice-Presidente', small: true}
+                ]
+           }      
+        ] 
+    }
+]
